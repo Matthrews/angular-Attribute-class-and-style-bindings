576f88179fb8cbff498749fb929752763f81c903
Matthrews <matthrews@outlook.com>
2022-05-15 18:55:46 +0800

    2 files modified

--- src/app/comp-with-host-binding.component.ts
+++ src/app/comp-with-host-binding.component.ts
@@ -15,7 +15,6 @@ export class CompWithHostBindingComponent {
   width = '200px';
 }
 
-
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
